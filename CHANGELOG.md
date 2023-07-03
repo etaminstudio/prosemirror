fa4fa812434505dbbc7ed0d8a1735c32fe1a4794
Hans Lemuet <hans@spone.net>
2023-07-03 22:32:43 +0200

    first commit

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## [Unreleased]
+
+## [0.0.1] - 2023-07-03
+
+- Initial release
---
+++ lib/prosemirror.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "prosemirror/version"
+
+module Prosemirror
+  class Error < StandardError; end
+  # Your code goes here...
+end
